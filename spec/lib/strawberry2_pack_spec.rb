74026e7afc12444822e723ce668876749c51b7ef
Akira Suenami <a.suenami@gmail.com>
2021-03-04 02:09:28 +0900

    テストリファクタリング
    前の課題でパラメタライズテストを学んでるのでここは結構もうすばやくいける
    例によって、この場合はサイズが何でもいいのでテストに工夫が（ｒｙ
    ここまできて気づいたけど重さを与えてインスタンス作るコンストラクタ、もう使わないｗ
    全部同じ、2つ同じ、全部違うという3パターンでテストしたけど、ここのテストケースどうするかはペアごとに違ってきそう

--- spec/lib/strawberry2_pack_spec.rb
+++ spec/lib/strawberry2_pack_spec.rb
@@ -1,19 +1,22 @@
 describe Strawberry2Pack do
   describe '#same_type_all?' do
-    it do
-      strawberry1 = Strawberry2.factory('あまおう', size: 'LL')
-      strawberry2 = Strawberry2.factory('あまおう', size: 'L')
-      strawberry3 = Strawberry2.factory('あまおう', size: 'M')
-      pack = Strawberry2Pack.new(strawberry1, strawberry2, strawberry3)
-      expect(pack).to be_same_type_all
-    end
-
-    it do
-      strawberry1 = Strawberry2.factory('あまおう', size: 'LL')
-      strawberry2 = Strawberry2.factory('とちおとめ', size: 'L')
-      strawberry3 = Strawberry2.factory('さがほのか', size: 'M')
-      pack = Strawberry2Pack.new(strawberry1, strawberry2, strawberry3)
-      expect(pack).not_to be_same_type_all
+    [
+      [ 'あまおう', 'あまおう', 'あまおう', 'すべて同じ場合', true ],
+      [ 'あまおう', 'あまおう', 'とちおとめ', '2つ同じ(1つ違う)場合', false ],
+      [ 'あまおう', 'とちおとめ', 'さがほのか', 'すべて違う場合', false ],
+    ].each do |type1, type2, type3, case_description, expected|
+      context "#{case_description}" do
+        it do
+          size1 = ['LL', 'L', 'M', 'S'].sample
+          size2 = ['LL', 'L', 'M', 'S'].sample
+          size3 = ['LL', 'L', 'M', 'S'].sample
+          strawberry1 = Strawberry2.factory(type1, size: size1)
+          strawberry2 = Strawberry2.factory(type2, size: size2)
+          strawberry3 = Strawberry2.factory(type3, size: size3)
+          pack = Strawberry2Pack.new(strawberry1, strawberry2, strawberry3)
+          expect(pack.same_type_all?).to eq expected
+        end
+      end
     end
   end
 end
